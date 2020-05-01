f31a3e14444833d28fc7ca7af02bbc6fd0b1ac4d
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-05-01 18:36:57 +0300

    Update help_text.py

--- ffmpegbot/plugins/help_text.py
+++ ffmpegbot/plugins/help_text.py
@@ -1,6 +1,11 @@
 from pyrogram import Client, Filters
 
 from ffmpegbot import (HELP_STICKER, MSAADA_TXT, TMP_DOWNLOAD_DIRECTORY)
+from telegram.ext import CommandHandler, Filters, MessageHandler, CallbackQueryHandler
+
+DONATE_STRING = """*Assalaam Aleykum* Habaari za Saahizi, Hii nisehemu inayo husu Utengenezaji [my creator](t.me/Twuwbaa).\
+Khamis Au Hamis Nimtengenezaji Wa Marobot Hapa Telegram Kama una hitaji Kutengenezewa Robot Kama hili onana na Mimia au nitafute Katika @Huduma
+"""
 
 @Client.on_message(Filters.command(["start"]))
 async def start_text(client, message):
@@ -9,3 +14,7 @@ async def start_text(client, message):
 @Client.on_message(Filters.command(["help"]))
 async def msaada(client, message):
     await message.reply_text(MSAADA_TXT, quote=True)
+
+khamis_handler = CommandHandler("khamis", donate) migrate_handler = MessageHandler(Filters.status_update.migrate, migrate_chats)
+
+dispatcher.add_handler(khamis_handler)
