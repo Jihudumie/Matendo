5fda861d23f6ea6dbd3a18047eebfc8fbe312592
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-05-01 18:17:27 +0300

    Update help_text.py

--- ffmpegbot/plugins/help_text.py
+++ ffmpegbot/plugins/help_text.py
@@ -2,10 +2,9 @@ from pyrogram import Client, Filters
 
 from ffmpegbot import (HELP_STICKER, MSAADA_TXT, TMP_DOWNLOAD_DIRECTORY)
 
-
 @Client.on_message(Filters.command(["start"]))
 async def start_text(client, message):
-    await message.reply_text(HELP_STICKER, quote=True)
+    await bot.send_message(HELP_STICKER, reply_to_message_id=update.message_id )
 
 @Client.on_message(Filters.command(["help"]))
 async def msaada(client, message):
