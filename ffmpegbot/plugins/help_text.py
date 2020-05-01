781199b9c5abf76af3fbef54966a7cc14bfe8295
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-05-01 08:57:58 +0300

    Update help_text.py

--- ffmpegbot/plugins/help_text.py
+++ ffmpegbot/plugins/help_text.py
@@ -9,3 +9,7 @@ from ffmpegbot import (
 @Client.on_message(Filters.command(["start"]))
 async def start_text(client, message):
     await message.reply_text(HELP_STICKER, quote=True)
+
+@Client.on_message(Filters.command(["help"]))
+async def help_msaada(client, message):
+    await message.reply_text(MSAADA, quote=True)
