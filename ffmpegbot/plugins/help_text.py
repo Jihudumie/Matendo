c89172e5f9c80986cce1c4be7c410a8020bcbfe5
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-05-16 20:32:49 +0300

    Update help_text.py

--- ffmpegbot/plugins/help_text.py
+++ ffmpegbot/plugins/help_text.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, Filters
 
-from ffmpegbot import (HELP_STICKER, MSAADA_TXT, TMP_DOWNLOAD_DIRECTORY)
+from ffmpegbot import (HELP_STICKER, MSAADA_TXT, TMP_DOWNLOAD_DIRECTORY, 000)
 
 
 @Client.on_message(Filters.command(["start"]))
