1c023819b0e0ebf97ec8357489ca344e9a0c856b
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-05-17 16:41:14 +0300

    Update help_text.py

--- ffmpegbot/plugins/help_text.py
+++ ffmpegbot/plugins/help_text.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, Filters
 
-from ffmpegbot import (HELP_STICKER, MSAADA_TXT, TMP_DOWNLOAD_DIRECTORY, UTANGULIZI)
+from ffmpegbot import (HELP_STICKER, MSAADA_TXT, TMP_DOWNLOAD_DIRECTORY, UI)
 4
 
 @Client.on_message(Filters.command(["start"]))
@@ -13,4 +13,4 @@ async def msaada(client, message):
 
 @Client.on_message(Filters.command(["000"]))
 async def uta_ngu(client, message):
-    await message.reply_text(UTANGULIZI, quote=True)
+    await message.reply_text(UI, quote=True)
